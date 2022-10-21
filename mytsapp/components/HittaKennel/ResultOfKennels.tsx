bed383a20b58304a60c984677e67f6691336a5fa
Fanny Reslow <reslowf@gmail.com>
2022-10-21 13:43:32 +0200

    database

--- mytsapp/components/HittaKennel/ResultOfKennels.tsx
+++ mytsapp/components/HittaKennel/ResultOfKennels.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 const people = [
   {
     name: "Lindsay Walton",
@@ -22,6 +27,8 @@ const people = [
 ];
 
 export default function ResultOfKennels() {
+  const [enabled, setEnabled] = useState(false);
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
@@ -31,6 +38,31 @@ export default function ResultOfKennels() {
             A list of all the users in your account including their name, title,
             email and role.
           </p>
+          <div>
+            <Switch.Group as="div" className="flex items-center justify-center">
+              <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className={classNames(
+                  enabled ? "bg-indigo-600" : "bg-gray-200",
+                  "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 "
+                )}
+              >
+                <span
+                  aria-hidden="true"
+                  className={classNames(
+                    enabled ? "translate-x-5" : "translate-x-0",
+                    "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+                  )}
+                />
+              </Switch>
+              <Switch.Label as="span" className="ml-3">
+                <span className="text-sm font-medium text-gray-900">
+                  {enabled ? "sluta bevaka" : "bevaka"}
+                </span>
+              </Switch.Label>
+            </Switch.Group>
+          </div>
         </div>
       </div>
       <div className="mt-8 flex flex-col">
